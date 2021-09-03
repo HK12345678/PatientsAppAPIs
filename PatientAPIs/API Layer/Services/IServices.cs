5462f258821b044cf9b4909df2bba5cbc63c3486
HK1234 <harikrishnadaxini123@gmail.com>
2021-09-03 16:25:55 +0530

    Added service class and one layer in between.

---
+++ PatientAPIs/API Layer/Services/IServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PatientAPIs.Models;
+namespace APIs
+{
+    public interface IServices<T> where T : PatientAPIs.Models.tblPatient
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object Id);
+        T Insert(T obj);
+        T Update(T obj);
+    }
+}
