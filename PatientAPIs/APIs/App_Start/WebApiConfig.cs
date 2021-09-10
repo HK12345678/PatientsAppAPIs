8ba0789695a61f32b2d5cb3005f8cd3509a97d90
HK1234 <harikrishnadaxini123@gmail.com>
2021-09-10 14:29:19 +0530

    Connection string at a API level only.

--- PatientAPIs/APIs/App_Start/WebApiConfig.cs
+++ PatientAPIs/APIs/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace APIs
@@ -12,8 +13,9 @@ namespace APIs
             // Web API configuration and services
 
             // Web API routes
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
             config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
